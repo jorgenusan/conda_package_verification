8e3d103063d94c74289482f41c8435328ba782c9
Jorge Núñez Santiago <jorge.nusan@gmail.com>
2024-04-02 00:45:40 +0200

    Refactor main function and fix code formatting

--- __main__.py
+++ __main__.py
@@ -3,15 +3,12 @@ from package_verification.core.imports_finder import ImportsFinder
 from package_verification.core.env_file_management import EnvFileManagement
 from pathlib import Path
 
-if __name__ == '__main__':
+def main():
     package_manager = PackageManagement()
     imports_finder = ImportsFinder()
     env_manager = EnvFileManagement()
     
-    
     root_path = Path('.')
-    env_path = env_manager.find_env_file(Path('.'))
-
     imports = imports_finder.get_all_imports(root_path)
     
     conda_packages = []
@@ -22,7 +19,9 @@ if __name__ == '__main__':
                 conda_packages.append(package_info)            
     
 
+    env_manager.find_env_file(root_path)
     env_manager.update_txt_file(conda_packages)
     env_manager.update_yaml_file(conda_packages)
-    
-            
+
+if __name__ == '__main__':
+    main()
