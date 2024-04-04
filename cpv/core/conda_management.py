72ba4e738cc9a5b58790081c950f875258d00087
Jorge Núñez Santiago <jorge.nusan@gmail.com>
2024-04-04 17:44:05 +0200

    Remove unused methods from CondaManagement class

--- cpv/core/conda_management.py
+++ cpv/core/conda_management.py
@@ -7,17 +7,6 @@ class CondaManagement:
     def __init__(self, env_name: str) -> None:
         self._env_name = env_name
 
-    def check_package(self, package_name):
-        try:
-            output = subprocess.run(
-                ["conda", "search", package_name], capture_output=True
-            )
-            if "No match" in output.stdout.decode():
-                return False
-            return True
-        except subprocess.CalledProcessError:
-            return False
-
     def get_package_info(self, package_name):
         try:
             output = subprocess.check_output(
@@ -30,20 +19,6 @@ class CondaManagement:
                 return None
             name, version = last_line[3].split()[:2]
             return {"name": name, "version": version}
-        except subprocess.CalledProcessError:
-            return None
-
-    def export_env_dependencies(self):
-        try:
-            output = subprocess.check_output(
-                ["conda", "env", "export", "-n", self._env_name, "--from-history"],
-                stderr=subprocess.STDOUT,
-            )
-            if not output:
-                return None
-            output_str = output.decode("utf-8")
-            output_dict = yaml.safe_load(output_str)
-            return [item.split("=")[0] for item in output_dict["dependencies"]]
         except subprocess.CalledProcessError:
             return None
 
