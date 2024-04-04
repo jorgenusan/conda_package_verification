4febb853449d9e08ff747ec962e9d1e57b623598
Jorge Núñez Santiago <jorge.nusan@gmail.com>
2024-04-04 17:45:41 +0200

    Update environment file with external imports

--- cpv/cpv_cli.py
+++ cpv/cpv_cli.py
@@ -5,6 +5,7 @@ from loguru import logger
 from cpv.core.conda_management import CondaManagement
 from cpv.core.env_file_management import EnvFileManagement
 from cpv.core.imports_finder import ImportsFinder
+from cpv.utils.utils import std_modules
 
 
 class CPVCLI:
@@ -17,15 +18,20 @@ class CPVCLI:
 
     def run(self) -> None:
         imports = self._imports_finder.get_all_imports(self._root_path)
+        external_imports = [
+            lib
+            for lib in imports
+            if lib not in std_modules() and not lib.startswith("_")
+        ]
         env_file_extension = self._env_file.suffix
 
         logger.info(f"Updating the environment file {self._env_file}")
         if env_file_extension == ".txt":
             self._env_manager.txt_file = self._env_file
-            self._update_txt_file(imports)
+            self._update_txt_file(external_imports)
         else:
             self._env_manager.yaml_file = self._env_file
-            self._update_yaml_file(imports)
+            self._update_yaml_file(external_imports)
 
     def _update_txt_file(self, imports: list) -> None:
         dependencies_to_add = []
@@ -43,25 +49,21 @@ class CPVCLI:
 
     def _update_yaml_file(self, imports: list) -> None:
         dependencies_to_add_yaml = []
-        yaml_dependencies = self._env_manager.get_yaml_dependencies()
 
-        logger.info("Getting dependencies from the environment")
-        conda_dependencies = self._conda_manager.export_env_dependencies()
         pip_dependencies = self._conda_manager.export_pip_dependencies()
+        if pip_dependencies:
+            pip_info = self._conda_manager.get_package_info("pip")
+            dependencies_to_add_yaml.append(pip_info)
 
         logger.info("Checking dependencies in the yaml file")
-        for dependency in conda_dependencies:
-            if dependency not in yaml_dependencies:
-                package_info = self._conda_manager.get_package_info(dependency)
+        pip_packages = []
+        for imp in imports:
+            package_info = self._conda_manager.get_package_info(imp)
+            if imp not in pip_dependencies:
                 if package_info:
                     dependencies_to_add_yaml.append(package_info)
-
-        pip_packages = []
-        for dependency in pip_dependencies:
-            if dependency not in yaml_dependencies and dependency in imports:
-                package_info = self._conda_manager.get_package_info(dependency)
-                if package_info:
-                    pip_packages.append(package_info)
+            else:
+                pip_packages.append(package_info)
 
         logger.info(f"Updating {self._env_manager.yaml_file} file")
         self._env_manager.update_yaml_file(dependencies_to_add_yaml, pip_packages)
