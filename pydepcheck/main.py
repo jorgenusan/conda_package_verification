33be023f9820ff8c662790c461c4a6f22026c18b
Jorge Núñez Santiago <jorge.nusan@gmail.com>
2024-05-09 16:29:37 +0200

    Move file hash check to separate function in main.py

--- pydepcheck/main.py
+++ pydepcheck/main.py
@@ -1,12 +1,11 @@
 import argparse
 import sys
 
-from utils.utils import get_file_hash
-
 from pydepcheck.cli import CLI
+from pydepcheck.utils.utils import get_file_hash
 
 
-def main() -> None:
+def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description="Update environment files")
     parser.add_argument(
         "-ef", "--env_file", type=str, help="Path to the environment file"
@@ -15,14 +14,19 @@ def main() -> None:
     parser.add_argument("files", nargs="*", help="Files to be committed (ignored)")
     args = parser.parse_args()
 
+    return args
+
+
+def main() -> None:
+    args = parse_args()
+
     initial_hash = get_file_hash(args.env_file)
 
     cpv_cli = CLI(args.env_file, args.env_name)
     cpv_cli.run()
 
     final_hash = get_file_hash(args.env_file)
-    print(initial_hash)
-    print(final_hash)
+
     if initial_hash != final_hash:
         sys.exit(1)
 
